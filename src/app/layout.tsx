f6233aa7f6ba437c0a330346b1341bbfb27c0fc6
rdev014 <rdev01431@gmail.com>
2026-07-04 00:07:42 +0530

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="light">
+      <head><meta name="google-site-verification" content="VHq7fomHpPljqOb-aPSFUl5eLui9N6TtfS_aNqUkh3k" /></head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        {/* <Toaster position="top-right" richColors /> */}
+      </body>
+    </html>
+  );
+}
