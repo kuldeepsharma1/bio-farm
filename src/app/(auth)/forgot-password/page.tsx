d11dd534309b14a8753ff8733b2da192a604048e
rdev014 <rdev01431@gmail.com>
2026-07-16 23:39:06 +0530

    bug fix

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -13,7 +13,7 @@ export default function ForgotPassword() {
 
     try {
       const res = await fetch("/api/auth/forgot-password", {
-        method: POST,
+        method: "POST",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify({email}),
       });
